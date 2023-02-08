db4796228940367c330300c88291d38a7203b964
Muhammad Rival <em.rival282@gmail.com>
2023-02-08 08:25:48 +0700

    pajak pph21

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use App\Calculate\FirstRuleCalculator;
+use App\Calculate\SecondRuleCalculator;
+use App\Calculate\ThirdRuleCalculator;
+use App\Calculate\FourthRuleCalculator;
+use App\Calculate\PPH21Calculator;
+
+
+
+// require_once 'App/init.php';
+
+$first = new FirstRuleCalculator();
+$second = new SecondRuleCalculator($first);
+$third = new ThirdRuleCalculator($second);
+$fourth = new FourthRuleCalculator($third);
+
+$calculator = new PPH21Calculator($first, $second, $third, $fourth);
+//1.250.000
+echo $calculator->calculate(50000000);
+echo PHP_EOL;
